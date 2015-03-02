18f4da22f93bdd17057905d01284f1273b6f80b8
rnons <remotenonsense@gmail.com>
2015-03-02 12:24:10 +0800

    Comment out setMaxDimensions, adjust css

--- source/js/classes/crop-host.js
+++ source/js/classes/crop-host.js
@@ -89,7 +89,7 @@ crop.factory('cropHost', ['$document', 'cropAreaCircle', 'cropAreaSquare', 'crop
           canvasDims[1]=minCanvasDims[1];
           canvasDims[0]=canvasDims[1]*imageRatio;
         }
-        elCanvas.prop('width',canvasDims[0]).prop('height',canvasDims[1]).css({'margin-left': -canvasDims[0]/2+'px', 'margin-top': -canvasDims[1]/2+'px'});
+        elCanvas.prop('width',canvasDims[0]).prop('height',canvasDims[1]).css({'margin-left': -canvasDims[0]/2+'px'});
 
         theArea.setX(ctx.canvas.width/2);
         theArea.setY(ctx.canvas.height/2);
